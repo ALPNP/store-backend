a266448331cfe9b56d66ee99dd01713a5077129e
filippov <filippov@teleformis.ru>
2018-03-05 15:59:17 +0300

    refactoring

--- app/routes/userCreatorRoute.js
+++ app/routes/userCreatorRoute.js
@@ -5,6 +5,7 @@ var ServerResponse = require('./../classes/ServerResponse');
 var utilities = require('./../utilities');
 
 userCreator.post('/userCreate', function (req, res) {
+    var sr = new ServerResponse();
     var userName = req.body.userName;
     var password = req.body.password;
     var email = req.body.email;
@@ -21,28 +22,23 @@ userCreator.post('/userCreate', function (req, res) {
             userName: userName
         }, function (err, user) {
             if (err) {
-                var findErrRes = new ServerResponse({success: false, message: err, code: 1});
-                res.json(findErrRes.error());
+                res.json(sr.setRes(false, err, 1).send());
             }
 
             if (!user) {
                 newUser.save(function (err) {
                     if (err) {
-                        var userSaveErrRes = new ServerResponse({success: false, message: err, code: 2});
-                        res.json(userSaveErrRes.error());
+                        res.json(sr.setRes(false, err, 2).send());
                     }
 
-                    var successRes = new ServerResponse({success: true, message: 'User saved', code: 3});
-                    res.json(successRes.success());
+                    res.json(sr.setRes(true, 'User saved', 3).send());
                 });
             } else {
-                var userAlreadyError = new ServerResponse({success: false, message: 'User not saved', code: 4});
-                res.json(userAlreadyError.error());
+                res.json(sr.setRes(false, 'User not saved', 4).send());
             }
         });
     } else {
-        var userAlreadyError = new ServerResponse({success: false, message: 'Incorrect user', code: 5});
-        res.json(userAlreadyError.error());
+        res.json(sr.setRes(false, 'Incorrect user', 5).send());
     }
 });
 
